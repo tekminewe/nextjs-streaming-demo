e0799c0852eb4faed6cc9096e7792fadd49cd660
Ewe Tek Min <ewe.tekmin@outlook.com>
2024-09-15 22:43:21 +0100

    feat: add error handling

--- src/components/similar-products.tsx
+++ src/components/similar-products.tsx
@@ -2,9 +2,17 @@ import { Product } from "@/app/types";
 import { ProductListItem } from "./product-list-item";
 
 export const SimilarProducts = async ({ id }: { id: number }) => {
-  const products: Product[] = await fetch(
-    `http://localhost:3000/api/products/${id}/similar`
-  ).then((res) => res.json());
+  let products: Product[] = [];
+  try {
+    products = await fetch(
+      `http://localhost:3000/api/products/${id}/similar`
+    ).then((res) => res.json());
+    // Uncomment to simulate error
+    // throw new Error("Simulated error for testing");
+  } catch (e) {
+    console.error("Error fetching similar products:", e);
+    return <div>Failed to load similar products. Please refresh the page</div>;
+  }
 
   return (
     <div className="grid grid-cols-3 gap-4">
